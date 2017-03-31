bdc1f71413a6d9c36b9678d90732614aeee0019d
wangweiwei <will_workhard@163.com>
2017-04-01 00:38:04 +0800

    根模块

--- todo-list/TodoList/src/app/app.module.ts
+++ todo-list/TodoList/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import { AuthService } from './services/auth.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +18,9 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    {provide: "auth", useClass: AuthService}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
