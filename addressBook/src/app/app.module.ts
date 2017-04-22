733d18c7dd3614601aee6857aa4836210b66d2dd
wangweiwei <will_workhard@163.com>
2017-04-22 17:51:23 +0800

    header&footer

--- addressBook/src/app/app.module.ts
+++ addressBook/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CollectionComponent } from './collection/collection.component';
 import { DetailComponent } from './detail/detail.component';
 import { EditComponent } from './edit/edit.component';
 import { ListComponent } from './list/list.component';
+
+import { rootRouterConfig } from './app.router';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,8 @@ import { ListComponent } from './list/list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(rootRouterConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- addressBook/src/app/app.router.ts
+++ addressBook/src/app/app.router.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
 import { CollectionComponent } from './collection';
+import { DetailComponent } from './detail';
+import { EditComponent } from './edit';
+import { ListComponent } from './list';
+
+export const rootRouterConfig: Routes = [
+    {
+        path: "",
+        redirectTo: "list",
+        pathMatch: "full"
+    },
+    {
+        path: 'list',
+        component: ListComponent
+    },
+    {
+        path: 'list/:id',
+        component: DetailComponent
+    },
+    {
+        path: 'edit',
+        component: EditComponent
+    },
+    {
+        path: 'edit/:id',
+        component: EditComponent
+    },
+    {
+        path: 'collection',
+        component: CollectionComponent
+    }
+];
