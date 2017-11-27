98798d384aa74d7e9ea5296b0e73d86a8db9062c
wangweiwei <will_workhard@163.com>
2017-11-27 14:15:12 +0800

    ckeditor

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CkeditorComponent } from './ckeditor/ckeditor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CkeditorComponent
   ],
   imports: [
     BrowserModule,
---
+++ demo/src/app/ckeditor/ckeditor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ckeditor',
+  templateUrl: './ckeditor.component.html',
+  styleUrls: ['./ckeditor.component.css']
+})
+export class CkeditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
